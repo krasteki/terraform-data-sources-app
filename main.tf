299330b11f7a108b6ef72a918710571e9cc837ee
krasteki <kiril.krastev@hashicorp.com>
2022-03-01 15:27:08 +0200

    add remote data source from vpc repo

--- main.tf
+++ main.tf
@@ -5,9 +5,18 @@ terraform {
     }
   }
 }
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../terraform-data-sources-vpc/terraform.tfstate"
+  }
+}
+
+
 
 provider "aws" {
-  region = "us-east-1"
+  region = data.terraform_remote_state.vpc.outputs.aws_region
 }
 
 resource "random_string" "lb_id" {
@@ -24,8 +33,8 @@ module "elb_http" {
 
   internal = false
 
-  security_groups = []
-  subnets         = []
+  security_groups = data.terraform_remote_state.vpc.outputs.lb_security_group_ids
+  subnets         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
 
   number_of_instances = length(aws_instance.app)
   instances           = aws_instance.app.*.id
@@ -46,13 +55,24 @@ module "elb_http" {
   }
 }
 
-resource "aws_instance" "app" {
-  ami = "ami-04d29b6f966df1537"
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+resource "aws_instance" "app" {
+  ami = data.aws_ami.amazon_linux.id
+  count = var.instances_per_subnet * length(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
   instance_type = var.instance_type
 
-  subnet_id              = ""
-  vpc_security_group_ids = []
+  subnet_id              = data.terraform_remote_state.vpc.outputs.private_subnet_ids[count.index % length(data.terraform_remote_state.vpc.outputs.private_subnet_ids)]
+  vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.app_security_group_ids
 
   user_data = <<-EOF
     #!/bin/bash
